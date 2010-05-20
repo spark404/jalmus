88e15a4ed7a961c8fbca44555e8c5eb1d4f3200f
crichard <crichard@a73d39a2-691c-0410-9cb9-8a9fc645279b>
2010-05-20 18:54:59 +0000

    New class to store answer in new rhythm game
    git-svn-id: https://svn.berlios.de/svnroot/repos/jalmus/trunk@71 a73d39a2-691c-0410-9cb9-8a9fc645279b

---
+++ src/net/jalmus/RhythmAnswer.java
@@ -0,0 +1,64 @@
+/**
+ * 
+ */
+package net.jalmus;
+
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+/**
+ * @author christophe
+ *
+ */
+public class RhythmAnswer {
+	  int posx; // position of the point
+      int posy;
+      boolean good; // 
+	/**
+	 * 
+	 */
+	public RhythmAnswer(int x, int y, boolean good) {
+		this.posx = x;
+		this.posy = y;
+		this.good = good;
+
+	}
+	
+	public void init() {
+		this.posx = -1;
+		this.posy = -1;
+		this.good = true;
+
+	}
+	
+	public boolean isnull(){
+		return this.posx == -1;
+	}
+	
+    public void setPosx(int x) {
+        this.posx = x;
+      }
+
+      public int getPosx() {
+        return this.posx;
+      }
+      
+      public void setPosy(int y) {
+          this.posx = y;
+        }
+
+        public int getPosy() {
+          return this.posy;
+        }
+	
+	
+        public void paint(Graphics g){
+        	  Color cr = new Color(238, 0, 0);
+              Color cg = new Color(152, 251, 152);
+              if (good) g.setColor(cg);
+              else g.setColor(cr);
+              g.fillOval(this.posx, this.posy, 10,10);
+        }
+}
