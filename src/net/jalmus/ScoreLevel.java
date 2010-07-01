6db23001e337122b328084537cd8bd627a12b447
crichard <crichard@a73d39a2-691c-0410-9cb9-8a9fc645279b>
2010-07-01 20:47:46 +0000

    Choosing notes to work function of tonality
    git-svn-id: https://svn.berlios.de/svnroot/repos/jalmus/trunk@134 a73d39a2-691c-0410-9cb9-8a9fc645279b

--- src/net/jalmus/ScoreLevel.java
+++ src/net/jalmus/ScoreLevel.java
@@ -68,15 +68,34 @@ public void initpitchtab(){
 	for (int i=0; i<pitchtab.length-1; i++) {
 		   pitchtab[i] = 0;
 	}
+	
 
-	pitchtab[0]=64;
-	pitchtab[1]=65;
-	pitchtab[2]=67;
-	pitchtab[3]=69;
-	pitchtab[4]=71;
-	pitchtab[5]=72;
-	pitchtab[6]=74;
-	pitchtab[7]=76;
+		pitchtab[0]=64;
+		pitchtab[1]=65;
+		pitchtab[2]=67;
+		pitchtab[3]=69;
+		pitchtab[4]=71;
+		pitchtab[5]=72;
+		pitchtab[6]=74;
+		pitchtab[7]=76;
+	
+	if (this.currenttonality.issharp() && this.currenttonality.getNbalt() >=  1) pitchtab[1]=66;
+	if (this.currenttonality.issharp() && this.currenttonality.getNbalt() >=  2) pitchtab[5]=73;
+	if (this.currenttonality.issharp() && this.currenttonality.getNbalt() >=  3) pitchtab[2]=68;
+	if (this.currenttonality.issharp() && this.currenttonality.getNbalt() >=  4) pitchtab[6]=75;
+	if (this.currenttonality.issharp() && this.currenttonality.getNbalt() >=  5) pitchtab[3]=70;
+	if (this.currenttonality.issharp() && this.currenttonality.getNbalt() >=  6) { pitchtab[0]=65; pitchtab[7]=77;}
+	if (this.currenttonality.issharp() && this.currenttonality.getNbalt() >=  7) pitchtab[4]=72;
+	
+	if (this.currenttonality.isflat() && this.currenttonality.getNbalt() >=  1) pitchtab[4]=70;
+	if (this.currenttonality.isflat() && this.currenttonality.getNbalt() >=  2) {pitchtab[0]=63; pitchtab[7]=75;}
+	if (this.currenttonality.isflat() && this.currenttonality.getNbalt() >=  3) pitchtab[3]=68;
+	if (this.currenttonality.isflat() && this.currenttonality.getNbalt() >=  4) pitchtab[6]=73;
+	if (this.currenttonality.isflat() && this.currenttonality.getNbalt() >=  5) pitchtab[2]=66;
+	if (this.currenttonality.isflat() && this.currenttonality.getNbalt() >=  6) pitchtab[5]=71;
+	if (this.currenttonality.isflat() && this.currenttonality.getNbalt() >=  7) pitchtab[1]=64;
+		
+	
 	
 	}
 	
@@ -95,6 +114,22 @@ public void initpitchtab(){
 		pitchtab[6]=41;
 		pitchtab[7]=43;
 		
+		if (this.currenttonality.issharp() && this.currenttonality.getNbalt() >=  1) pitchtab[6]=42;
+		if (this.currenttonality.issharp() && this.currenttonality.getNbalt() >=  2) pitchtab[3]=37;
+		if (this.currenttonality.issharp() && this.currenttonality.getNbalt() >=  3) {pitchtab[0]=32;  pitchtab[7]=44; }
+		if (this.currenttonality.issharp() && this.currenttonality.getNbalt() >=  4) pitchtab[4]=39;
+		if (this.currenttonality.issharp() && this.currenttonality.getNbalt() >=  5) pitchtab[1]=34;
+		if (this.currenttonality.issharp() && this.currenttonality.getNbalt() >=  6) pitchtab[5]=41; 
+		if (this.currenttonality.issharp() && this.currenttonality.getNbalt() >=  7) pitchtab[2]=36;
+		
+		if (this.currenttonality.isflat() && this.currenttonality.getNbalt() >=  1) pitchtab[2]=34;
+		if (this.currenttonality.isflat() && this.currenttonality.getNbalt() >=  2) pitchtab[5]=39;
+		if (this.currenttonality.isflat() && this.currenttonality.getNbalt() >=  3) pitchtab[1]=32;
+		if (this.currenttonality.isflat() && this.currenttonality.getNbalt() >=  4) pitchtab[4]=37;
+		if (this.currenttonality.isflat() && this.currenttonality.getNbalt() >=  5) {pitchtab[0]=30; pitchtab[7]=42;}
+		if (this.currenttonality.isflat() && this.currenttonality.getNbalt() >=  6) pitchtab[3]=35;
+		if (this.currenttonality.isflat() && this.currenttonality.getNbalt() >=  7) pitchtab[6]=40;
+			
 		}
 	
 }
